a471f634cc6622c38e5499f17fdee9bda50454c2
Gio Shathaia <gioshathaia@Gios-MacBook-Air.local>
2023-03-06 13:26:08 -0500

    will randomly assort images when clicked

--- src/App.js
+++ src/App.js
@@ -1,31 +1,48 @@
 import React from 'react';
 import './App.css';
 import { useState } from 'react';
-import Siberian from './siberian.png';
+import Siberian from './images/siberian.png';
+import Tabby from './images/tabby.png';
 
 function App() {
   let [cardArr, nextCard] = useState(new Array(20).fill(0));
 
-// reverse function order
+// useEffect
 function checkImage(e) {
-  const selectedIndex = parseInt(e.target.id);
-  console.log(selectedIndex);
-  if (selectedIndex === cardArr[selectedIndex]) {
+  const clickedVal = parseInt(e.target.id);
+  console.log(clickedVal);
+  if (clickedVal === cardArr[clickedVal]) {
     nextCard(cardArr = new Array(20).fill(0));
     console.log(cardArr);
+    RandomAssort();
   }
   else {ImageClicked(e)};
 }
 
 function ImageClicked(e) {
-  const selectedIndex = parseInt(e.target.id);
-  cardArr.splice(selectedIndex, 1, selectedIndex);
+  const clickedVal = parseInt(e.target.id);
+  cardArr.splice(clickedVal, 1, clickedVal);
   console.log(cardArr);
+  RandomAssort();
+}
+
+function RandomAssort() {
+  // let [imgArr, nextImg] = useState([Siberian, Tabby]);
+
+   const container = document.querySelector('.container');
+   console.log(container)
+   for (let i = container.children.length; i >= 0; i--) {
+      container.appendChild(container.children[Math.random() * i | 0]);
+   }
 }
 
 
+// how will you re arrange the images?
   return (
-    <img className='siberian' alt='' src={Siberian} id='12' onClick={checkImage}></img>
+    <div className='container'>
+      <img className='siberian' alt='' src={Siberian} id='1' onClick={ImageClicked}></img>
+      <img className='tabby' alt='' src={Tabby} id='2' onClick={ImageClicked}></img>
+    </div>
   )
 
 }
