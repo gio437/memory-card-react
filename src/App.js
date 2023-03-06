c3b9029b4a4fa086dcd51f2178c58c6d65508857
Gio Shathaia <gioshathaia@Gios-MacBook-Air.local>
2023-03-06 11:24:09 -0500

    appends image to array when clicked

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { useState } from 'react';
+import Siberian from './siberian.png';
 
 function App() {
+  let [cardArr, nextCard] = useState(new Array(20).fill(0));
+
+// reverse function order
+function checkImage(e) {
+  const selectedIndex = parseInt(e.target.id);
+  console.log(selectedIndex);
+  if (selectedIndex === cardArr[selectedIndex]) {
+    nextCard(cardArr = new Array(20).fill(0));
+    console.log(cardArr);
+  }
+  else {ImageClicked(e)};
+}
+
+function ImageClicked(e) {
+  const selectedIndex = parseInt(e.target.id);
+  cardArr.splice(selectedIndex, 1, selectedIndex);
+  console.log(cardArr);
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <img className='siberian' alt='' src={Siberian} id='12' onClick={checkImage}></img>
+  )
+
 }
 
 export default App;
---
+++ src/cards.js
@@ -0,0 +1,3 @@
+// import React from 'react';
+// import Siberian from '../src/siberian.png';
+// import App from './App';
